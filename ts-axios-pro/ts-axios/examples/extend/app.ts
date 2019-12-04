e175f8fb4dbb2691eb16a3577366219db63f5d7f
yyccQQu <643929860@qq.com>
2019-12-04 08:59:07 +0700

    webpack配合node的使用 以及axios基础请求编写

---
+++ ts-axios-pro/ts-axios/examples/extend/app.ts
@@ -0,0 +1,70 @@
+import axios from '../../src/index'
+
+axios({
+  url: '/extend/post',
+  method: 'post',
+  data: {
+    msg: 'hi'
+  }
+})
+
+axios.request({
+  url: '/extend/post',
+  method: 'post',
+  data: {
+    msg: 'hello'
+  }
+})
+
+axios.get('/extend/get')
+
+axios.options('/extend/options')
+
+axios.delete('/extend/delete')
+
+axios.head('/extend/head')
+
+axios.post('/extend/post', { msg: 'post' })
+
+axios.put('/extend/put', { msg: 'put' })
+
+axios.patch('/extend/patch', { msg: 'patch' })
+
+axios('/extend/post', {
+  method: 'post',
+  data: {
+    msg: 'hi'
+  }
+})
+
+interface ResponseData<T = any> {
+  code: number
+  result: T
+  message: string
+}
+
+interface User {
+  name: string
+  age: number
+}
+
+function getUser<T>() {
+  return axios<ResponseData<T>>('/extend/user')
+    .then(res => res.data)
+    .catch(err => console.error(err))
+}
+
+async function text() {
+  // user 被推断出为
+  // {
+  //  code: number,
+  //  result: { name: string, age: number },
+  //  message: string
+  // }
+  const user = await getUser<User>()
+  if (user) {
+    console.log(user.result)
+  }
+}
+
+text()
---
+++ ts-axios-pro/ts-axios/src/types/index.ts
@@ -0,0 +1,15 @@
+
+export type Method = 'get' | 'GET'
+| 'delete' | 'Delete'
+| 'head' | 'HEAD'
+| 'options' | 'OPTIONS'
+| 'post' | 'POST'
+| 'put' | "PUT"
+| 'patch' | 'PATCH'
+
+export interface AxiosRequestConfig {
+    url: string
+    method?: string
+    data?: any
+    params?: any
+}
